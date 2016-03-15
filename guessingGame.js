f3a18b4da2c0a6f1b8cd84071cadb20c22486743
Torng-Lin Taffy Chen <TorngLinChen@Torng-Lins-iMac.local>
2016-03-14 22:36:41 -0400

    fixed hint, playAgain

--- guessingGame.js
+++ guessingGame.js
@@ -2,33 +2,31 @@
 // try to elminate these global variables in your project, these are here just to start.
 
 var playersGuess;
-    winningNumber = generateWinningNumber();
-    guessArr = [];
-    guessCounts = 0;
-    timesLeft = 5;
+var winningNumber = generateRandomNumber();
+var guessArr = [];
+var guessCounts = 0;
+var timesLeft = 5;
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
 
-function generateWinningNumber(){
-    return Math.floor(Math.random()*(100 - 1)) + 1;
+function generateRandomNumber(){
+    return Math.floor(Math.random()*(100 - 1 +1)) + 1;
 }
 
 // Fetch the Players Guess
 
-function playersGuessSubmission(){
-	// add code here
-    
+function playersGuessSubmission(){    
     playersGuess = parseInt($("#input").val());
     $("input").val("");
     checkGuess();
     playersGuess = 0;
-    
-    timesLeft --;
+
     if (timesLeft > 0){
         $("#timesLeft").text(timesLeft + " times left");
     }else{
         $("#timesLeft").text("sorry, try again!");
+        $("#submit").attr("disabled", "disabled");
     }
     
 }
@@ -75,6 +73,7 @@ function checkGuess(){
 
     if (guessArr.indexOf(playersGuess) === -1){
         guessCounts++;
+        timesLeft--;
         }else{
         $("#msg").text("you just guessed the same number...");
         }
@@ -83,47 +82,42 @@ function checkGuess(){
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
+function generateHintArr(num){
+    var hintArr = [];
+    var notWinningNum = generateRandomNumber();
+    hintArr.push(winningNumber);
+    while (hintArr.length < num){
+        if (hintArr.indexOf(notWinningNum) === -1){
+                hintArr.push(notWinningNum);
+        } else {
+                notWinningNum = generateRandomNumber();
+        }
+    }
+    return hintArr.sort(function(a,b){
+        return a-b;
+    })
+}
+
+function generateHintMsg(num){
+    $("#msg").text("the Winning Number is one of these " + num + ": " + generateHintArr(num));
+}
 
 function provideHint(){
-    var hintArr = [];
-    if (timesLeft === 5){
-        while (hintArr.length <9){
-            hintArr.push(Math.floor(Math.random()*(100 - 1)) + 1);
-            hintArr.push(winningNumber);
-        }
-        $("#msg").text("the Winning Number is one of these 10: " + hintArr);
-    }else if(timesLeft === 4){
-        while (hintArr.length <7){
-            hintArr.push(Math.floor(Math.random()*(100 - 1)) + 1);
-            hintArr.push(winningNumber);
-        }
-        $("#msg").text("the Winning Number is one of these 8: " + hintArr);
-    }else if(timesLeft === 3){
-        while (hintArr.length <5){
-            hintArr.push(Math.floor(Math.random()*(100 - 1)) + 1);
-            hintArr.push(winningNumber);
-        }
-        $("#msg").text("the Winning Number is one of these 6: " + hintArr);
-    }else if(timesLeft === 2){
-        while (hintArr.length <3){
-            hintArr.push(Math.floor(Math.random()*(100 - 1)) + 1);
-            hintArr.push(winningNumber);
-        }
-        $("#msg").text("the Winning Number is one of these 4: " + hintArr);
-    }else if(timesLeft === 1){
-        while (hintArr.length <1){
-            hintArr.push(Math.floor(Math.random()*(100 - 1)) + 1);
-            hintArr.push(winningNumber);
-        }
-        $("#msg").text("the Winning Number is one of these 2: " + hintArr);
-    }
+    generateHintMsg(timesLeft*2);
     
 }
 
 // Allow the "Player" to Play Again
 
 function playAgain(){
-	// add code here
+    playersGuess = 0;
+    winningNumber = generateWinningNumber();
+    guessArr = [];
+    guessCounts = 0;
+    timesLeft = 5;
+    $("#msg").text("");
+    $("#timesLeft").text("5 times left");
+    $("#submit").removeAttr("disabled");
 }
 
 
